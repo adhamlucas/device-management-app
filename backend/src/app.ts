a33d956f7631cbc007cb2c62de6146e396691630
adhamlucas <adhamlucas20@gmail.com>
2025-02-28 12:48:58 -0400

    chore(backend): add cors to the api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,12 @@
 import express, { Application } from 'express';
-
+import cors from 'cors';
 import categoryRoutes from './category'
 import deviceRoutes from './device';
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/category', categoryRoutes);
