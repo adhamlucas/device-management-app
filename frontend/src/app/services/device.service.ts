74e0ac5dd3256af3724a781154311b6800d2eeea
adhamlucas <adhamlucas20@gmail.com>
2025-02-28 13:05:17 -0400

    feat(device-service): add service method to fetch all devices from API

---
+++ frontend/src/app/services/device.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Device } from '../models/device.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeviceService {
+  private baseUrl = 'http://localhost:8000';
+  private apiUrl = `${this.baseUrl}/device`;
+
+  constructor(private http: HttpClient) { }
+
+  getDevices(): Observable<Device[]> {
+    return this.http.get<Device[]>(this.apiUrl);
+  }
+}
