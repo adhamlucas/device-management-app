f2c911989a3fa63c031854c6c75cc3a7536fd3a4
adhamlucas <adhamlucas20@gmail.com>
2025-02-28 13:05:51 -0400

    feat(device): rendering fetched devices on the table

--- frontend/src/app/pages/device/device.component.ts
+++ frontend/src/app/pages/device/device.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatButton } from '@angular/material/button'
+import { MatIcon } from '@angular/material/icon'
+import { Device } from '../../models/device.model';
+import { DeviceService } from '../../services/device.service';
+
 @Component({
   selector: 'app-device',
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatButton,
+    MatIcon
+  ],
   templateUrl: './device.component.html',
   styleUrl: './device.component.css'
 })
-export class DeviceComponent {
+export class DeviceComponent implements OnInit {
+  devices: Device[] = []
+  constructor(private deviceService: DeviceService) {}
+  
+  ngOnInit(): void {
+    this.loadDevices();
+  }
+
+  loadDevices(): void {
+    this.deviceService.getDevices().subscribe({
+      next: (devices) => this.devices = devices
+    })
+  }
+
+  displayedColumns: string[] = ['id', 'categoryId', 'color', 'partNumber', 'actions']
 }
